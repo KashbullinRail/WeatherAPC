01ecc4a450576c5089087bfcd11755dd616cbb38
KashbullinRail <kashbullinrail@gmail.com>
2022-09-24 11:57:56 +0300

    создана WindActivity.kt и xml добавлены компоненты: text, fab создан интент для запуска WindActivity

--- app/src/main/java/com/example/weatherapc/MainActivity.kt
+++ app/src/main/java/com/example/weatherapc/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.weatherapc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -21,9 +22,10 @@ class MainActivity() : AppCompatActivity() {
 
     private val viewModel: WeatherScreenViewModel by viewModel()
 
-    private val tvHello : TextView by lazy { findViewById(R.id.tVHello) }
-    private val fabWeather : FloatingActionButton by lazy { findViewById(R.id.fabWeatherFetch)}
+    private val tvHello : TextView by lazy { findViewById(R.id.tVTemp) }
+    private val fabWeather : FloatingActionButton by lazy { findViewById(R.id.fabWeatherFetchTemp)}
     private val progressBar : ProgressBar by lazy { findViewById(R.id.progressBar)}
+    private val fabWeatherWindAction: FloatingActionButton by lazy { findViewById(R.id.fabWeatherWindAction) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,6 +36,12 @@ class MainActivity() : AppCompatActivity() {
         fabWeather.setOnClickListener {
             viewModel.processUIEvent(UIEvent.onButtonClicked)
         }
+
+        fabWeatherWindAction.setOnClickListener{
+            val windActivityAction = Intent(this, WindActivity::class.java)
+            startActivity(windActivityAction)
+        }
+
 
     }
 
